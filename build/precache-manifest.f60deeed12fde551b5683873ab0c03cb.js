f68afcf40b0d2c4c3b8b4986b21d2259173cb2f1
ArturoZilliRios <artuurozilli00@gmail.com>
2021-06-20 20:04:44 -0500

    finish proyect v2

---
+++ build/precache-manifest.f60deeed12fde551b5683873ab0c03cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e36542e9630a6e547d14",
+    "url": "/static/js/main.e2b761e8.chunk.js"
+  },
+  {
+    "revision": "541aa1b522f56c60652c",
+    "url": "/static/js/2.fafa7853.chunk.js"
+  },
+  {
+    "revision": "e36542e9630a6e547d14",
+    "url": "/static/css/main.ec78ca8d.chunk.css"
+  },
+  {
+    "revision": "df31913b2937264ffbbf4a84931933e8",
+    "url": "/index.html"
+  }
+];
